d457d27a0341d14129d9cd86440271814d3456e4
CaroleSinou <32440327+CaroleSinou@users.noreply.github.com>
2021-01-26 17:34:19 -0500

    Update config.js

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -3,5 +3,5 @@ var siteTheme = gbifReactComponents.themeBuilder.extend({baseTheme: 'light', ext
 }});
 
 var siteConfig = {
-  "rootPredicate": {"type": "in","key": "COUNTRY","values": ["US","CA","MX"]}}
+  "rootPredicate": {"type": "in","key": "COUNTRY","values": ["US","CA","MX"]}
 };
