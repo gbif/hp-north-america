3b8e2880c7821e50572fd5e30e62cc0ebf1d01b0
CaroleSinou <32440327+CaroleSinou@users.noreply.github.com>
2021-01-26 13:20:35 -0500

    Changed scoped to North America

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -3,5 +3,5 @@ var siteTheme = gbifReactComponents.themeBuilder.extend({baseTheme: 'light', ext
 }});
 
 var siteConfig = {
-  rootPredicate: { type: 'equals', key: 'country', value: "US" }
+  rootPredicate: { type: 'in', key: 'country', values: ["US", "CA", "MX", "UM", "PR", "VI", "AS", "GU", "MP"]}
 };
