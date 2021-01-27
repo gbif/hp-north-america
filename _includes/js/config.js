2b65f1bd776c5ed88a733dbb058c8d3d99f3f735
David Jennings <jenningsdt@users.noreply.github.com>
2021-01-27 08:46:03 -0800

    Try changing to gadm_gid

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -6,15 +6,15 @@ var siteConfig = {
   rootPredicate: {
     type: 'or',
     predicates: [
-      { type: 'equals', key: 'countryCode', value: "US" },
-      { type: 'equals', key: 'countryCode', value: "CA" },
-      { type: 'equals', key: 'countryCode', value: "MX" },
-      { type: 'equals', key: 'countryCode', value: "UM" },
-      { type: 'equals', key: 'countryCode', value: "PR" },
-      { type: 'equals', key: 'countryCode', value: "VI" },
-      { type: 'equals', key: 'countryCode', value: "AS" },
-      { type: 'equals', key: 'countryCode', value: "GU" },
-      { type: 'equals', key: 'countryCode', value: "MP" }
+      { type: 'equals', key: 'gadm_gid', value: "US" },
+      { type: 'equals', key: 'gadm_gid', value: "CA" },
+      { type: 'equals', key: 'gadm_gid', value: "MX" },
+      { type: 'equals', key: 'gadm_gid', value: "UM" },
+      { type: 'equals', key: 'gadm_gid', value: "PR" },
+      { type: 'equals', key: 'gadm_gid', value: "VI" },
+      { type: 'equals', key: 'gadm_gid', value: "AS" },
+      { type: 'equals', key: 'gadm_gid', value: "GU" },
+      { type: 'equals', key: 'gadm_gid', value: "MP" }
     ]
   }
 };
