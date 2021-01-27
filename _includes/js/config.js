a5b82ed93e2b94e09449245e1e445bd37f0e8451
David Jennings <jenningsdt@users.noreply.github.com>
2021-01-27 08:26:02 -0800

    Expanding the scope

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -9,6 +9,12 @@ var siteConfig = {
       { type: 'equals', key: 'countryCode', value: "US" },
       { type: 'equals', key: 'countryCode', value: "CA" },
       { type: 'equals', key: 'countryCode', value: "MX" },
+      { type: 'equals', key: 'countryCode', value: "UM" },
+      { type: 'equals', key: 'countryCode', value: "PR" },
+      { type: 'equals', key: 'countryCode', value: "VI" },
+      { type: 'equals', key: 'countryCode', value: "AS" },
+      { type: 'equals', key: 'countryCode', value: "GU" },
+      { type: 'equals', key: 'countryCode', value: "MP" }
     ]
   }
 };
