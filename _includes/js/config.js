cab75f1fac53cbe2722d9184c1bd1ddf6c0d4d96
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2022-02-18 13:56:58 +0100

    add default map focus

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -4,20 +4,27 @@ var siteTheme = gbifReactComponents.themeBuilder.extend({baseTheme: 'light', ext
 }});
 
 var siteConfig = {
-  rootPredicate: {
-     "type": "and",
-     "predicates": [
-        {
-         "type": "in",
-         "key": "country",
-         "values": ["US", "CA", "MX", "UM", "PR", "VI", "AS", "GU", "MP"]
-       },
-       {
-         "type": "equals",
-         "key": "notIssues",
-         "value": "COUNTRY_COORDINATE_MISMATCH"
-       }
-     ]
-   },
-  occurrenceSearchTabs: ['MAP', 'TABLE', 'GALLERY', 'DATASETS']
+  occurrence: {
+    mapSettings: {
+      lat: 60.0344351910359,
+      lng: -111.28467879517733,
+      zoom: 0.9115440763665068
+    }
+    rootPredicate: {
+       "type": "and",
+       "predicates": [
+          {
+           "type": "in",
+           "key": "country",
+           "values": ["US", "CA", "MX", "UM", "PR", "VI", "AS", "GU", "MP"]
+         },
+         {
+           "type": "equals",
+           "key": "notIssues",
+           "value": "COUNTRY_COORDINATE_MISMATCH"
+         }
+       ]
+     },
+    occurrenceSearchTabs: ['MAP', 'TABLE', 'GALLERY', 'DATASETS']
+  }
 };
